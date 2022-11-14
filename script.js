3b367ca68792656f38a59b733a81b55b05fddbc4
Seyit Enes <seyit7enes@gmail.com>
2022-11-14 21:44:05 +0100

    done not great styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 const reviews = [
   {
-    id: 1,
+    id: "review1",
     imgSource:
       "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80",
     name: "Susanne Hall",
@@ -10,7 +10,7 @@ const reviews = [
       "I received my Patagonia jacket as a birthday gift and I love it! I wear it year-round and in all weather conditions – I have it on as I'm writing this right now. I love the colors, the style, and the fit. It's an excellent jacket and I can't imagine wearing anything else.",
   },
   {
-    id: 2,
+    id: "review2",
     imgSource:
       "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     name: "Michael Johnson",
@@ -19,7 +19,7 @@ const reviews = [
       "This jacket is exactly what I needed! I live in Texas, and get around a lot in the heat. I'm not usually a fan of jackets, but I purchased this one a few weeks ago and I love it. It's very warm and comfy, and I can wear it all day without overheating. The only negative is that it doesn't come with a rain cover, but that's just a minor fault.",
   },
   {
-    id: 3,
+    id: "review3",
     imgSource:
       "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
     name: "Selena Bisbing",
@@ -28,7 +28,7 @@ const reviews = [
       "Hands down my favorite winter jacket! Super warm and comfortable. Typically I wear a size large but because it is a parka, I went a size down and it fits perfect. I really wish Patagonia would make this jacket in Navy Blue or Dark Gray",
   },
   {
-    id: 4,
+    id: "review4",
     imgSource:
       "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
     name: "Justin Luke",
@@ -42,9 +42,39 @@ const card = document.querySelector("#slider");
 const img = document.querySelector("#imgSource");
 const personName = document.querySelector("#name");
 const verified = document.querySelector("#verified");
-const review = document.querySelector("#comment");
-
+const text = document.querySelector("#comment");
 const prevBtn = document.querySelector(".left-icon");
 const nextBtn = document.querySelector(".right-icon");
 
-nextBtn.addEventListener("click", () => {});
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  img.src = item.imgSource;
+  personName.textContent = item.name;
+  verified.textContent = item.verified;
+  text.textContent = item.comment;
+});
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.imgSource;
+  personName.textContent = item.name;
+  verified.textContent = item.verified;
+  text.textContent = item.comment;
+}
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
